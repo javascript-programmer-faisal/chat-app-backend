bb94b60c30513564f892f75d755a5c72cd4cad42
mern-coder-faisal <js.dev.faisal@gmail.com>
2025-06-01 03:26:34 +0600

    builddeploy2

--- src/index.js
+++ src/index.js
@@ -15,10 +15,7 @@ const __dirname = path.resolve();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error);
